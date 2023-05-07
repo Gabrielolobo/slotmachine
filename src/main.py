fb942e1a62df7d956a186393b048460fa322963a
Gabriel Lobo <gabrielolobo42@gmail.com>
2023-05-07 17:46:37 -0300

    get bet lines

--- src/main.py
+++ src/main.py
@@ -1,3 +1,7 @@
+
+MAX_LINES = 3
+
+
 def deposit():
     while True:
         amount = input("How much would you like to deposit: R$")
@@ -6,11 +10,31 @@ def deposit():
             if amount > 0:
                 break
             else:
-                print("Please enter a positive value")
+                print("Please enter a positive value.")
         else:
-            print("Please enter a number")
+            print("Please enter a number.")
 
     return amount
 
 
-deposit()
+def get_number_of_lines():
+    while True:
+        lines = input(
+            "Enter the number of line you would like to bet on (1-" + str(MAX_LINES) + ")? ")
+        if lines.isdigit():
+            lines = int(lines)
+            if 1 <= lines <= MAX_LINES:
+                break
+            else:
+                print("Please enter a positive value.")
+        else:
+            print("Please enter a number.")
+
+    return lines
+
+
+def main():
+    balance = deposit()
+
+
+main()
